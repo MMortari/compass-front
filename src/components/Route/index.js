4b4fc3f59eb7415349399d4c5129f0cc9c42a716
Mortari <bmortari2000@gmail.com>
2019-09-30 22:44:48 -0300

    Rotas privadas e ajuste no login

---
+++ src/components/Route/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Route as RouteRouter, Redirect } from 'react-router-dom';
+
+export default function Route({
+  component: Component,
+  isPrivate = false,
+  ...rest
+}) {
+  let logged;
+
+  const local = localStorage.getItem('compassUser');
+  const user = JSON.parse(local);
+
+  console.log("User -> ", user, local);
+
+  if(user && user.auth) {
+    logged = true;
+  } else {
+    logged = false;
+  }
+
+  console.log("Authentication -> ", !!user)
+
+  if(!logged && isPrivate) {
+    // Será redirecionado para o login
+    return <Redirect to="/login" />;
+  }
+  
+  if(logged && !isPrivate) {
+    // Será redirecionado para o main
+    return <Redirect to="/" />;
+  }
+
+  return <RouteRouter {...rest} component={Component} />
+
+}
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { 
+  BrowserRouter, 
+  Switch, 
+  // Route 
+} from 'react-router-dom';
 
 // Components
 import Header from './components/Header/index';
+import Route from './components/Route';
 // Pages
 import Principal from './pages/Principal';
 import Login from './pages/Login';
@@ -18,20 +23,20 @@ function Routes() {
         <Route path="/login" component={Login} />
 
 
-        <Route path="/">
+        <Route path="/" isPrivate>
           <Header />
           {/* Principal */}
-          <Route exact path="/" component={Principal} />
+          <Route exact path="/" component={Principal} isPrivate />
 
           {/* Questions */}
-          <Route exact path="/questions" component={Questions} />
-          <Route exact path="/questions/:question" component={Questions} />
+          <Route exact path="/questions" component={Questions} isPrivate />
+          <Route exact path="/questions/:question" component={Questions} isPrivate />
 
           {/* Profile */}
-          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/profile" component={Profile} isPrivate />
 
           {/* Thank */}
-          <Route exact path="/thank" component={Thank} />
+          <Route exact path="/thank" component={Thank} isPrivate />
         </Route>
       </Switch>
     </BrowserRouter>
